c10ba5696a7456d5dc21dafee803d9f368ed18bc
paull <paullopisparrilla@gmail.com>
2024-12-17 12:14:36 +0000

    Model class User created.

---
+++ src/main/java/software/ulpgc/model/User.java
@@ -0,0 +1,44 @@
+package software.ulpgc.model;
+
+public class User {
+
+    public enum Gender{Male, Female}
+
+    private final String name;
+    private final String surname;
+    private final String email;
+    private final String gender;
+    private byte[] photo;
+
+    public User(String name, String surname, String email, String gender) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public User setPhoto(byte[] photo) {
+        this.photo = photo;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+}
