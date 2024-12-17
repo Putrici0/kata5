68ad8ea0386b70fd498f50e3cb9c5aef86dc541d
paull <paullopisparrilla@gmail.com>
2024-12-17 12:44:30 +0000

    Main created. All files tested.

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -1,4 +1,19 @@
 package software.ulpgc;
 
+import software.ulpgc.control.LoadRandomUserCommand;
+import software.ulpgc.model.User;
+import software.ulpgc.view.RandomUserSwingApp;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        LoadRandomUserCommand command = new LoadRandomUserCommand(100);
+        command.execute();
+
+        for (User user : command.getUsers()) {
+            System.out.println(user.getName());
+        }
+        SwingUtilities.invokeLater(RandomUserSwingApp::new);
+    }
 }
