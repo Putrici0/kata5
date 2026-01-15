37f43bb28534029ae78a78b6be40a29758b1fe2e
paull <paullopisparrilla@gmail.com>
2026-01-15 12:06:44 +0000

    Base from Kata4. Store interfaces created.

---
+++ src/main/java/software/ulpgc/kata5/architecture/chores/HistogramBuilder.java
@@ -0,0 +1,45 @@
+package software.ulpgc.kata5.architecture.chores;
+
+import software.ulpgc.kata5.architecture.model.Movie;
+import software.ulpgc.kata5.architecture.viewmodel.Histogram;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class HistogramBuilder {
+    private final Stream<Movie> movies;
+    private final Map<String, String> labels;
+
+    private HistogramBuilder(Stream<Movie> movies) {
+        this.movies = movies;
+        this.labels = new HashMap<>();
+    }
+
+    public static HistogramBuilder with(Stream<Movie> movies) {
+        return new HistogramBuilder(movies);
+    }
+
+    public HistogramBuilder title(String title) {
+        labels.put("title", title);
+        return this;
+    }
+
+    public HistogramBuilder x(String x) {
+        this.labels.put("x", x);
+        return this;
+    }
+
+    public HistogramBuilder legend(String legend) {
+        this.labels.put("legend", legend);
+        return this;
+    }
+
+    public Histogram build(Function<Movie, Integer> binarize) {
+        Histogram histogram = new Histogram(labels);
+        movies.map(binarize).forEach(histogram::add);
+        return histogram;
+    }
+
+}
